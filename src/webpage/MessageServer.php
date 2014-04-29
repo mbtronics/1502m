8a8e02c3d8d65bae67be8583c19567c2b47903a5
Blomme, Maarten <Maarten.Blomme@flir.com>
2014-04-29 13:49:48 +0200

    Add DisplayClient status on MessageServer page

--- src/webpage/MessageServer.php
+++ src/webpage/MessageServer.php
@@ -3,6 +3,9 @@
 # The file we use to store messages
 $MessageFile = "messages.txt";
 
+# The file in which we store the timestamp from the latest request
+$TimestampFile = "timestamp.txt";
+
 # The (arbitrary) message length
 $MaxMessageLength = 100;
 
@@ -29,6 +32,37 @@ if (Isset($_GET['messages']))
 		unlink($MessageFile);
 	}
 
+	# Write timestamp to file
+	file_put_contents($TimestampFile, time());
+
+	exit;
+}
+
+# User has asked DisplayClient status
+if (Isset($_GET['status']))
+{
+	$Status = False;
+	if (file_exists($TimestampFile))
+	{
+		# Read timestamp from file
+		$Timestamp = intval(file_get_contents($TimestampFile));
+
+		# Check if timestamp is less then 5s ago
+		if ($Timestamp >= time()-5)
+		{
+			$Status = True;	# Last connection was less then 5s ago, status is good!
+		}
+	}
+
+	if ($Status)
+	{
+		echo "online";
+	}
+	else
+	{
+		echo "offline";
+	}
+
 	exit;
 }
 
@@ -46,23 +80,29 @@ if (Isset($_FILES["live_jpg"]))
 <head>
 	<title>BUDA::lab Electronics Lab</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="led.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 </head>
 
 <body>
-
+<!--
 <div class="header">
 	<img src="BUDALAB-LOGO7-300x102.png">
 	<p><b><i>Electronics Lab</i></b></p>
 </div>
-
+-->
 <div class="body">
 	<div align="center">
 		<p>Send messages to our 1502m LED display!</p>
+		DisplayClient status: <span id="status">offline</span>
+		<div id="statusled" style="display: inline-block;" class="led led-red"></div> 
+
 		<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 			<input type="text" name="message" maxlength="<?php echo $MaxMessageLength; ?>" size="30">
 			<input type="submit" name="submit" value="Submit">
 		</form>
 		<br>
+
 		<canvas id="mjpeg" width="640px" height="480px" style="border:1px solid #d3d3d3"></canvas>
 		<script language="JavaScript">
 			var Mjpeg = document.getElementById('mjpeg').getContext('2d');
@@ -74,10 +114,31 @@ if (Isset($_FILES["live_jpg"]))
 
 		  	Img.src = "live.jpg";
 
-			window.setInterval("refreshCanvas()", 500);
-			function refreshCanvas(){
+			window.setInterval("doTimedStuff()", 500);
+
+			function doTimedStuff() {
+				refreshCanvas();
+				updateStatus();
+			};
+
+			function refreshCanvas() {
 				Img.src = "live.jpg?" + Date.now();
-				Mjpeg.drawImage(img, 0, 0);
+				Mjpeg.drawImage(Img, 0, 0);
+			};
+
+			function updateStatus() {
+				jQuery.get("<?php echo $_SERVER['PHP_SELF']; ?>?status&" + Date.now(), function(response) {
+					$("#status").html(response);
+					if (response == "online") {
+						$("#statusled").removeClass('led-red');
+						$("#statusled").addClass('led-green');
+					}
+					else
+					{
+						$("#statusled").removeClass('led-green');
+						$("#statusled").toggleClass('led-red');
+					}
+				});
 			};
 		</script>
 	</div>
